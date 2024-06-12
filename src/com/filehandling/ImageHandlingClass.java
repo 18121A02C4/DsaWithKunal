b77b07762f5aec4741cbefbffc4bc17ff188aecd
Shashank Peta <shashankreddypeta@gmail.com>
2024-06-12 22:39:17 +0530

    aaded outstream code in imagehandling.java

--- src/com/filehandling/ImageHandlingClass.java
+++ src/com/filehandling/ImageHandlingClass.java
@@ -15,12 +15,28 @@ public class ImageHandlingClass {
 //            System.out.println(e.getMessage());
 //        }
 //        System.out.println("Image copied successfully");
+//        try{
+//            BufferedInputStream fis=new BufferedInputStream(new FileInputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\Shashank.jpg"));
+//            BufferedOutputStream fos=new BufferedOutputStream(new FileOutputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\new_Shashank.jpg"));
+//            int read;
+//            while((read=fis.read())!=-1){
+//                fos.write(read);
+//            }
+//        }catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println("Image copied successfully");
+
+
+
         try{
-            BufferedInputStream fis=new BufferedInputStream(new FileInputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\Shashank.jpg"));
-            BufferedOutputStream fos=new BufferedOutputStream(new FileOutputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\new_Shashank.jpg"));
+            BufferedInputStream fis=new BufferedInputStream(System.in);
+            System.out.println("enter the data");
+            BufferedOutputStream fos=new BufferedOutputStream(System.out);
             int read;
             while((read=fis.read())!=-1){
                 fos.write(read);
+                fos.flush();
             }
         }catch(IOException e){
             System.out.println(e.getMessage());
--- src/com/filehandling/kunal.java
+++ src/com/filehandling/kunal.java
@@ -7,15 +7,16 @@ public class kunal {
     public static void main(String[] args) throws IOException {
         System.out.println("शशांक");
         FileReader in = new FileReader(
-                "C:\\Users\\shash\\OneDrive\\Desktop\\filehandlingjava\\com.com.filehandling.kunal\\text.txt");
+                "C:\\\\Users\\\\shash\\\\intellijideaworkspace\\\\PractiseDsa\\\\src\\\\com\\\\filehandling\\\\text.txt");
 //        FileInputStream in = new FileInputStream(
-//                "C:\\Users\\shash\\OneDrive\\Desktop\\filehandlingjava\\com.com.filehandling.kunal\\text.txt");
+//                "C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\text.txt");
         int c;
         String ans="";
         while ((c = in.read()) != -1) {
 //            Writer writer = new OutputStreamWriter(System.out);
 //            writer.write(c); // Correctly writes the Unicode character
 //            writer.flush();
+            System.out.println(c);
            ans+=(char)c;
 
             // System.out.println(c);
