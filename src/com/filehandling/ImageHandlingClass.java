bf1e5c7f0af44123ae29db15638fb7c4de42012a
Shashank Peta <shashankreddypeta@gmail.com>
2024-06-12 20:10:11 +0530

    add files regarding imagehandling and photos obtained through it

---
+++ src/com/filehandling/ImageHandlingClass.java
@@ -0,0 +1,31 @@
+package com.filehandling;
+
+import java.io.*;
+
+public class ImageHandlingClass {
+    public static void main(String[] args) throws IOException {
+//        try{
+//            FileInputStream fis=new FileInputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\photo.jpg");
+//            FileOutputStream fos=new FileOutputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\new_photo.jpg");
+//            int read;
+//            while((read=fis.read())!=-1){
+//                fos.write(read);
+//            }
+//        }catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println("Image copied successfully");
+        try{
+            BufferedInputStream fis=new BufferedInputStream(new FileInputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\Shashank.jpg"));
+            BufferedOutputStream fos=new BufferedOutputStream(new FileOutputStream("C:\\Users\\shash\\intellijideaworkspace\\PractiseDsa\\src\\com\\filehandling\\new_Shashank.jpg"));
+            int read;
+            while((read=fis.read())!=-1){
+                fos.write(read);
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Image copied successfully");
+
+    }
+}
